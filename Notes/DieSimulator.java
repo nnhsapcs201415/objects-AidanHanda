d606d7d25d8b3e2d30bee112c8c31eb92a8ceb4a
Aidan Handa <akhanda@stu.naperville203.org>
2016-08-29 11:24:37 -0500

    8-29-16

---
+++ Notes/DieSimulator.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Write a description of class AddTester here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class DieSimulator
+{
+    public static void main(String[] args) {
+       Random rand = new Random();
+       System.out.println(rand.nextInt(6)+1);
+    }
+}
