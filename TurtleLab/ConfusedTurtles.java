191397f678428eef057927ab844559fc5de7b5e0
Aidan Handa <handa.aidan@gmail.com>
2016-08-31 21:33:36 -0500

    8-31-16

--- TurtleLab/ConfusedTurtles.java
+++ TurtleLab/ConfusedTurtles.java
@@ -13,27 +13,11 @@ public class ConfusedTurtles
 {
 
     public static void main(String[] args) {
-        int numTurtles = 360;
         World turtleWorld = new World(1920,1080);
-        List<Turtle> turtles = new ArrayList<Turtle>();
-        for(int count = 0; count<numTurtles; count++) {
-            turtles.add(new Turtle(turtleWorld));
-        }
-        for(int indexTurtles = 0; indexTurtles < numTurtles; indexTurtles++) {
-           
-            int colorSafe = indexTurtles%255;
-
-            turtles.get(indexTurtles).setPenWidth(indexTurtles/10);
-
-            turtles.get(indexTurtles).setPenColor(rainbowColor(numTurtles,indexTurtles));
-            //For black and white new Color(Math.abs(colorSafe),Math.abs(colorSafe),Math.abs(colorSafe))
-            System.out.println(turtles.get(indexTurtles).getPenColor());
-            turtles.get(indexTurtles).penDown();
-            turtles.get(indexTurtles).turn(indexTurtles);
-            turtles.get(indexTurtles).forward(indexTurtles);
-            turtles.get(indexTurtles).hide();
-            
-        }
+        funWithTurtles(45,1500,turtleWorld);
+        //funWithTurtles(135,1000,turtleWorld);
+        //funWithTurtles(225,1000,turtleWorld);
+        //funWithTurtles(315,1000,turtleWorld);
     }
 
     //RainbowMode
@@ -41,4 +25,31 @@ public class ConfusedTurtles
         return Color.getHSBColor(current / (total + 0.0f),1.0f,1f);
     }
     
+    private static void funWithTurtles(int beginRotation, int numTurtles, World turtleWorld) {
+
+        List<Turtle> turtles = new ArrayList<Turtle>();
+        for(int count = 0; count<numTurtles; count++) {
+            Turtle toAdd = new Turtle(turtleWorld);
+            toAdd.turn(beginRotation);
+            turtles.add(toAdd);
+        }
+        for(int indexTurtles = 0; indexTurtles < numTurtles; indexTurtles++) {
+            Turtle workingTurtle = turtles.get(indexTurtles);
+            int colorSafe = indexTurtles%255;
+
+            //turtles.get(indexTurtles).setPenWidth(indexTurtles/10);
+
+            workingTurtle.setPenColor(rainbowColor(numTurtles,indexTurtles));
+            workingTurtle.penDown();
+
+            for(int penWidthCalc = 1; penWidthCalc < numTurtles - indexTurtles; penWidthCalc++) {
+                if(penWidthCalc%10 == 0) workingTurtle.setPenWidth(penWidthCalc/10);
+                workingTurtle.turn(indexTurtles);
+                workingTurtle.forward(2);
+            }
+            //workingTurtle.forward(numTurtles - indexTurtles);
+            workingTurtle.hide();
+            
+        }
+    }
 }
